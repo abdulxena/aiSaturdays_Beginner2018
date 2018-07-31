b58bd451dc1ade939d8714c12016486dce536e49
abdulxena <41937478+abdulxena@users.noreply.github.com>
2018-07-31 10:08:35 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,8 +2,8 @@ import random
 
 ourList = list()
 count = 0 
-while (count < 11):
-    ourList.append(random.randint(1,10))
+while (count < 5):
+    ourList.append(random.randint(1,5))
     count += 1
     
 ourList
